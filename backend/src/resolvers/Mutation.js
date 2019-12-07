bb5f6198ee357960df1091b621d6825cf4771172
Chad Saglam <saglam.chd@gmail.com>
2019-12-07 01:19:28 +0100

    Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // title: args.title,
+          // description: args.description ==> 
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
