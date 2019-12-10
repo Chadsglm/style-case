fc46cb29090f26361f9c3f6cff633e372fa4bfb4
Chad Saglam <saglam.chd@gmail.com>
2019-12-10 13:47:53 +0100

    builded fe and be

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a4a6383e7f7ca2999764";
+/******/ 	var hotCurrentHash = "b7286cbe19c21c4116ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
