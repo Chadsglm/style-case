20c3686db359e45d0b933e46f3d726495f11157f
Chad Saglam <saglam.chd@gmail.com>
2019-12-17 18:01:04 +0100

    Removing Cart Items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -266,6 +266,31 @@ const Mutations = {
       info
     );
   },
+
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `{ id, user { id }}`
+    );
+    // 1.1 Make sure we found an item
+    if (!cartItem) throw new Error('No CartItem Found!');
+    // 2. Make sure they own that cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheating huhhhh');
+    }
+    // 3. Delete that cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
