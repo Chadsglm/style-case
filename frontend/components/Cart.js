8c259167badaaf0b46fd82b459fb2d617d48d0c8
Chad Saglam <saglam.chd@gmail.com>
2019-12-17 16:37:05 +0100

    Displaying Cart Items and Totals

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatMoney from '../lib/formatMoney';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import StyleButton from './styles/StyleButton';
+import User from './User';
+import CartItem from './CartItem';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -19,28 +23,44 @@ const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({ data }) => console.log(data) || (
-          <CartStyles open={data.cartOpen}>
+  <User>
+    {({ data: { me } }) => {
+      if (!me) return null;
+      console.log(me);
+      return ( 
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+        {toggleCart => (
+          <Query query={LOCAL_STATE_QUERY}>
+          {({ data }) => console.log(data) || (
+            <CartStyles open={data.cartOpen}>
             <header>
-              <CloseButton onClick={toggleCart} title="close">
-                &times;
-              </CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You Have __ Items in your cart.</p>
+            <CloseButton onClick={toggleCart} title="close">
+            &times;
+            </CloseButton>
+            <Supreme>{me.name}'s Cart</Supreme>
+            <p>
+              You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.
+            </p>
             </header>
 
+            <ul>
+              {me.cart.map(
+                cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />
+              )}
+            </ul>
+            
             <footer>
-              <p>€10.10</p>
-              <StyleButton>Checkout</StyleButton>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <StyleButton>Checkout</StyleButton>
             </footer>
-          </CartStyles>
+            </CartStyles>
+          )}
+          </Query>
         )}
-      </Query>
-    )}
-  </Mutation>
+        </Mutation>
+      );
+    }}
+  </User>
 );
 
 export default Cart;
