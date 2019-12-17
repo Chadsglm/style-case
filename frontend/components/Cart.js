e1b8de7838c7a3391256122f3f5f99a487e4d490
Chad Saglam <saglam.chd@gmail.com>
2019-12-17 11:23:27 +0100

    Apollo Local State Queries and Mutations

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import StyleButton from './styles/StyleButton';
 
-const Cart = () => (
-  <CartStyles open>
-    <header>
-      <CloseButton title="close">
-        &times;
-      </CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You Have __ Items in your cart.</p>
-    </header>
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
 
-    <footer>
-      <p>€10.10</p>
-      <StyleButton>Checkout</StyleButton>
-    </footer>
-  </CartStyles>
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
+const Cart = () => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => console.log(data) || (
+          <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton onClick={toggleCart} title="close">
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You Have __ Items in your cart.</p>
+            </header>
+
+            <footer>
+              <p>€10.10</p>
+              <StyleButton>Checkout</StyleButton>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
 );
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
